aee1dac37b52c20984eeb8f6faeb15f1339a0d1a
Marianoda20027 <163364385+Marianoda20027@users.noreply.github.com>
2024-11-08 14:49:46 -0600

    Delete frontend/demo-reactjs directory

--- frontend/demo-reactjs/src/hooks/useAppStore.ts
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-import { sessionStore, SessionStore } from './store/session.store';
-
-type AppStore = SessionStore;
-export const useAppStore = create<AppStore>(set => ({
-	...sessionStore(set),
-}));
