420473eff3d83a79f53605cc2e71ae1854b34e14
Marianoda20027 <163364385+Marianoda20027@users.noreply.github.com>
2024-11-08 14:49:04 -0600

    Arreglo

---
+++ demo-reactjs/src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import './App.css';
+import Details from './Details';
+
+function App() {
+	const [name, setName] = useState('');
+  const [names, setNames] = useState<string[]>([]);
+  const addName = () => {
+
+   
+    setNames([...names, name]); 
+    console.log(names); 
+
+  };
+
+
+	return (
+    <>
+    <div>{name}</div>
+    <div>
+      <input
+      type="text"
+      value={name} onChange={(e) => setName(e.target.value)}/>
+      <button onClick={addName}>Agregar</button>
+
+      <Details names ={names}/>
+     </div>
+     
+
+   </>
+  );
+}
+
+export default App;
